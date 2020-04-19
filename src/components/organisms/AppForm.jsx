b8a5cf41fd5145d5d258ea57254ec087c1b3bab2
Yuta Miyazaki <yuta.apple85@yahoo.ne.jp>
2020-04-19 09:56:04 +0900

    new GatsbySite

---
+++ src/components/organisms/AppForm.jsx
@@ -0,0 +1,19 @@
+ import React from 'react';
+ import FloatingButton from '../atoms/FloatingButton';
+ import styled from 'styled-components';
+
+ const AppForm = (props) => {
+     const {
+
+     } = props;
+
+     return (
+         <>
+            <FloatingButton
+                onClick={() => {}}
+            />
+         </>
+     );
+ }
+
+ export default AppForm;
--- src/pages/app.js
+++ src/pages/app.js
@@ -4,10 +4,11 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-import { makestyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { fade } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import AppForm from '../components/organisms/AppForm';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -32,5 +33,71 @@ const useStyles = makeStyles((theme) => ({
         },
         marginLeft: 0,
         width: '100%',
-    }
-}))
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
+export default function App() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="open drawer"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        学年費計算App
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search..."
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                            />
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <AppForm />
+        </div>
+    );
+}
