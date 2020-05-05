be63d76e4cb45bb98542b458963788d733bbbd5b
Yuta Miyazaki <yuta.apple85@yahoo.ne.jp>
2020-05-06 00:28:28 +0900

    fixed Lists

---
+++ src/components/Lists.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+function ListItemLink(props) {
+    return <List button component="a" {...props} />
+}
+
+export default function Lists() {
+    const classes = useStyles();
+
+    return (
+        <>
+            <div className={classes.root}>
+                <List component="nav" aria-label="main maiilbox folders">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="inbox" />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <DraftsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Drafts" />
+                    </ListItem>
+                </List>
+                <Divider />
+                <List component="nav" aria-label="secondary mailbox folders">
+                    <ListItem button>
+                        <ListItemText primary="Trash" />
+                    </ListItem>
+                    <ListItemLink href="#simple-list">
+                        <ListItemText primary="spam" />
+                    </ListItemLink>
+                </List>
+            </div>
+        </>
+    );
+}
